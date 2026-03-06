eed88b2ecb48869d5ac16bc02f2942a7c9632ad7
SeoHana0825 <onnuri1226@gmail.com>
2026-03-06 22:53:04 +0900

    refactor:JwtUtil 주석 정리

--- src/main/java/com/example/hungrypangproject/common/security/JwtUtil.java
+++ src/main/java/com/example/hungrypangproject/common/security/JwtUtil.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.accessibility.AccessibleAction;
 import javax.crypto.SecretKey;
 import java.util.Date;
 
@@ -18,17 +17,17 @@ import java.util.Date;
 public class JwtUtil {
 
     // 기본 셋팅
-    public static final String BEARER_PREFIX = "Bearer "; // Header
+    public static final String BEARER_PREFIX = "Bearer ";
     private static final long TOKEN_TIME = 60 * 60 * 1000L; // token 발급 시간 60분
 
     private SecretKey key;
     private JwtParser parser;
 
-    @Value("${jwt.secret.key}") // yaml 안에 있는 시크릿 키
+    @Value("${jwt.secret.key}")
     private String secretKeyString;
 
-    // jwt 라이브러리에서 필요한 내용들을 셋팅
-    @PostConstruct  // 어플리케이션이 시작될때 가장 먼저 시작되는 메소드 어노테이션
+    // jwt 라이브러리에서 필요한 내용들 셋팅
+    @PostConstruct
     public void init() {
         byte[] bytes = Decoders.BASE64.decode(secretKeyString);
         this.key = Keys.hmacShaKeyFor(bytes);
@@ -40,14 +39,14 @@ public class JwtUtil {
     // 토큰 발행 (생성)
     public String generateToken(String nickname,String email, String phoneNo, String address) {
         Date now = new Date();
-        return BEARER_PREFIX + Jwts.builder()// jwt 에서 제공해주는 builder로 만드는데
+        return BEARER_PREFIX + Jwts.builder()
                 .claim("nickname", nickname)
-                .claim("email", email) // 그 안에 이메일이라는 친구를 넣음
+                .claim("email", email)
                 .claim("phoneNo", phoneNo)
                 .claim("address", address)
-                .issuedAt(now) // 발행시간
-                .expiration(new Date(now.getTime() + TOKEN_TIME)) // 만료시간 지정
-                .signWith(key, Jwts.SIG.HS256) // 어떤 암호 키로, 어떤 알고리즘으로 만들 것인지 정보 셋팅
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + TOKEN_TIME))
+                .signWith(key, Jwts.SIG.HS256)
                 .compact();
     }
 
