bb426d072a6004c79c97d04e760d59eecfa8a36c
SeoHana0825 <onnuri1226@gmail.com>
2026-03-06 14:35:24 +0900

    refactor : grade, point enum 형식 변경

--- src/main/java/com/example/hungrypangproject/domain/membership/entity/GradeEnum.java
+++ src/main/java/com/example/hungrypangproject/domain/membership/entity/GradeEnum.java
@@ -1,12 +1,18 @@
 package com.example.hungrypangproject.domain.membership.entity;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum GradeEnum {
-    NORMAL,
-    VIP,
-    VVIP;
+    NORMAL("NORMAL","일반회원"),
+    VIP("VIP", "우수회원"),
+    VVIP("VVIP","최우수 회원");
+
+    private final String statusCode;
+    private final String description;
+
+    GradeEnum(String statusCode, String description) {
+        this.statusCode = statusCode;
+        this.description = description;
+    }
 }
--- src/main/java/com/example/hungrypangproject/domain/point/entity/PointEnum.java
+++ src/main/java/com/example/hungrypangproject/domain/point/entity/PointEnum.java
@@ -6,8 +6,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PointEnum {
-    USE,
-    HOLDING,
-    SAVE,
-    EXPIRE;
+    USE("USE","사용가능"),
+    HOLDING("HOLDING","대기중"),
+    SAVE("SAVE","누적"),
+    EXPIRE("EXPIRE","만료");
+
+    private final String statusCode;
+    private final String description;
+
+    PointEnum(String statusCode, String description) {
+        this.statusCode = statusCode;
+        this.description = description;
+    }
 }
