bf606f67ad1e42721c1a3ad1ea97c277c7afb7c6
SeoHana0825 <onnuri1226@gmail.com>
2026-03-09 15:02:20 +0900

    feat:SecurityConfig 객체 생성 및 JwtUtil, JwtFilter 주입

--- src/main/java/com/example/hungrypangproject/common/security/SecurityConfig.java
+++ src/main/java/com/example/hungrypangproject/common/security/SecurityConfig.java
@@ -2,11 +2,46 @@ package com.example.hungrypangproject.common.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    // memberService 인코딩 후 주석 삭제
+//    @Bean
+//    public PassswordEncoder passswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/member/signup", "/api/member/signin").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
