e6568f057bbc8491548bd7aedb42b29f3d8a6000
JJK3187 <truestory14@kakao.com>
2026-03-12 19:35:39 +0900

    refactor : order의 환불 가능 상태 변경(waiting > preparing)

--- src/main/java/com/example/hungrypangproject/domain/order/entity/Order.java
+++ src/main/java/com/example/hungrypangproject/domain/order/entity/Order.java
@@ -74,7 +74,7 @@ public class Order extends BaseEntity {
         if(!this.member.getMemberId().equals(userId)){
             throw new OrderException(ErrorCode.ORDER_CANCEL_FORBIDDEN);
         }
-        if(this.orderStatus != OrderStatus.WATING){
+        if(this.orderStatus != OrderStatus.PREPARING){
             throw new OrderException(ErrorCode.ORDER_NOT_CANCELABLE);
         }
         this.orderStatus = OrderStatus.REFUNDED;
