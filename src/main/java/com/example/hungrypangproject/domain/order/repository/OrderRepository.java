72d7f6874b0d7ea0afd0006184d45ac71f57b9fa
youzting <158013348+youzting@users.noreply.github.com>
2026-03-17 15:02:03 +0900

    feat: 캐싱

--- src/main/java/com/example/hungrypangproject/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/hungrypangproject/domain/order/repository/OrderRepository.java
@@ -21,5 +21,5 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT DISTINCT o FROM Order o LEFT JOIN FETCH o.orderItems WHERE o.id = :orderId")
     Optional<Order> findByIdWithItems(@Param("orderId") Long orderId);
 
-
+    long countByMemberMemberId(Long memberId);
 }
